43e36f15cee80b65b1d8d183006bf8eb15a1a07a
Wayne <wayne.nicholas@santa-laurensia.sch.id>
2026-05-23 13:10:58 +0700

    first upload

---
+++ script.js
@@ -0,0 +1,85 @@
+console.log("JS CONNECTED");
+
+function loadVotes() {
+
+  fetch("https://script.google.com/macros/s/AKfycbyLwoh6jQr9zVUBIcK7VG78IVMhIdGExVrpjrcwDRzzRKP__2yXiarzu341tXxLtWtc/exec")
+
+    .then(response => response.json())
+
+    .then(data => {
+
+      document.getElementById("vote1").innerText = data.paslon1;
+
+      document.getElementById("vote2").innerText = data.paslon2;
+
+      document.getElementById("vote3").innerText = data.paslon3;
+
+const total =
+  data.paslon1 +
+  data.paslon2 +
+  data.paslon3;
+
+const percent1 =
+  total > 0
+    ? ((data.paslon1 / total) * 100).toFixed(1)
+    : 0;
+
+const percent2 =
+  total > 0
+    ? ((data.paslon2 / total) * 100).toFixed(1)
+    : 0;
+
+const percent3 =
+  total > 0
+    ? ((data.paslon3 / total) * 100).toFixed(1)
+    : 0;
+
+document.getElementById("progress1").style.width =
+  percent1 + "%";
+
+document.getElementById("progress2").style.width =
+  percent2 + "%";
+
+document.getElementById("progress3").style.width =
+  percent3 + "%";
+
+document.getElementById("percent1").innerText =
+  percent1 + "%";
+
+document.getElementById("percent2").innerText =
+  percent2 + "%";
+
+document.getElementById("percent3").innerText =
+  percent3 + "%";
+
+      document.getElementById("card1").classList.remove("leading");
+      document.getElementById("card2").classList.remove("leading");
+      document.getElementById("card3").classList.remove("leading");
+
+      const votes = [
+        data.paslon1,
+        data.paslon2,
+        data.paslon3
+      ];
+
+      const maxVote = Math.max(...votes);
+
+      if (data.paslon1 === maxVote && maxVote > 0) {
+        document.getElementById("card1").classList.add("leading");
+      }
+
+      if (data.paslon2 === maxVote && maxVote > 0) {
+        document.getElementById("card2").classList.add("leading");
+      }
+
+      if (data.paslon3 === maxVote && maxVote > 0) {
+        document.getElementById("card3").classList.add("leading");
+      }
+
+    });
+
+}
+
+loadVotes();
+
+setInterval(loadVotes, 5000);
